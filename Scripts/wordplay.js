c4e76787a75dfabfa4d1e93268305fcbc81a895c
lourod1987 <louart87@gmail.com>
2017-09-01 11:08:05 -0400

    Adjusting Colors

--- Scripts/wordplay.js
+++ Scripts/wordplay.js
@@ -2,10 +2,10 @@ var red = [0, 100, 63];
 var orange = [40, 100, 60];
 var green = [75, 100, 40];
 var blue = [196, 77, 55];
-var purple = [280, 50, 60];
+var darkGreen = [17, 55, 33];
 
 var myName = "Luis Rodriguez";
-var letterColors = [red, orange, green, blue, purple];
+var letterColors = [red, orange, green, blue, darkGreen];
 if (10 > 3) {
     bubbleShape = "circle";
 } else {
