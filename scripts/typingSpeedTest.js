c67120102c1ef78b866260256f96ce98f03ecaa8
lourod1987 <louart87@gmail.com>
2017-10-15 22:59:23 -0400

    modify test typing text in html file, alter back to portfolio button to float on top of main content color, and add in all core  functionality for typing test js

--- scripts/typingSpeedTest.js
+++ scripts/typingSpeedTest.js
@@ -4,20 +4,83 @@ const originText = document.querySelector("#origin-text p").innerHTML;
 const resetButton = document.querySelector("#reset");
 const theTimer = document.querySelector(".timer");
 
+var timer = [0, 0, 0, 0];
+var interval;
+var timerRunning = false;
 
 // Add leading zero to numbers 9 or below (purely for aesthetics):
-
+function leadingZero(time) {
+    if (time <= 9) {
+        time = "0" + time;
+    }
+    return time;
+}
 
 // Run a standard minute/second/hundredths timer:
-
+function runTimer() {
+    let currentTime = leadingZero(timer[0]) + ":" + leadingZero(timer[1]) + ":" + leadingZero(timer[2]);
+    theTimer.innerHTML = currentTime;
+    timer[3]++;
+    
+    timer[0] = Math.floor((timer[3]/100)/60);
+    timer[1] = Math.floor((timer[3]/100) - (timer[0] * 60));
+    timer[2] = Math.floor(timer[3] - (timer[1] * 100) - (timer[0] * 6000));
+}
 
 // Match the text entered with the provided text on the page:
-
+function spellCheck() {
+    let textEntered = testArea.value;
+    let originTextMatch = originText.substring(0, textEntered.length);
+    
+    if (textEntered == originText) {
+        clearInterval(interval);
+        textWrappper.style.borderColor = "green";
+    } else {
+        if (textEntered == originTextMatch) {
+            textWrapper.style.borderColor = "#65CCf3";
+        } else {
+            textWrapper.style.borderColor = "red";
+        }
+    }
+    
+    console.log(textEntered);
+}
 
 // Start the timer:
-
+function start() {
+    let textEnteredLength = testArea.value.length;
+    
+    if (textEnteredLength === 0 && !timerRunning) {
+        timerRunning = true;
+        interval = setInterval(runTimer, 10);
+    }
+    
+    console.log(textEnteredLength);
+}
 
 // Reset everything:
+function reset() {
+    clearInterval(interval);
+    interval = null;
+    timer = [0, 0, 0, 0];
+    timerRunning = false;
+    
+    testArea.value = "";
+    theTimer.innerHTML = "00:00:00";
+    testWrapper.style.borderColor = "grey";
+    
+    console.log("reset button has been pressed!");
+}
 
 
 // Event listeners for keyboard input and the reset button:
+testArea.addEventListener("keypress", start, false);
+testArea.addEventListener("keyup", spellCheck, false);
+resetButton.addEventListener("click", reset, false);
+
+
+//Functionality to add:
+//Add words per minute count (find calculation online), grab time and combine with # of words in test.
+//Count number of errors the user makes (increment a number and display # of errors)
+//Add an array of different test texts so the user can switch between different text to test.
+//Add a high score board
