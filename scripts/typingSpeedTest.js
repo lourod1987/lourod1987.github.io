61865f4277ff14792bc0ed2710b61648a0783c10
lourod1987 <louart87@gmail.com>
2017-10-16 04:09:27 -0400

    adding metadata for displaying to user in html and getting behavior to work in js. Still need to fix accuracy and prevent text from being changed once test has begun unless it is reset.

--- scripts/typingSpeedTest.js
+++ scripts/typingSpeedTest.js
@@ -1,12 +1,41 @@
 const testWrapper = document.querySelector(".test-wrapper");
 const testArea = document.querySelector("#test-area");
-const originText = document.querySelector("#origin-text p").innerHTML;
+var originText = document.querySelector("#origin-text p").innerHTML;
 const resetButton = document.querySelector("#reset");
 const theTimer = document.querySelector(".timer");
 
+var wpmDisplay = document.querySelector("#wpm");
+var typingErrorDisplay = document.querySelector("#errors");
+var accuracyDisplay = document.querySelector("#accuracy");
+var changeOriginText = document.querySelector("#alter");
+
+var manyText = ["This is a typing test. Your goal is to duplicate the provided text, EXACTLY, in the field below. The timer starts when you start typing, and only stops when you match this text exactly. Do your best now. Good Luck!", "Do not lose time on daily trivialities. Do not dwell on petty detail, for all these things melt away and drift apart within the obscure traffic of time. Live well and live broadly. You are alive and living now. Now is the envy of all of the dead.", "Hello world. It is a nice day."];
+var arrCount = 0;
 var timer = [0, 0, 0, 0];
 var interval;
 var timerRunning = false;
+var wordsPerMinCalc;
+var typingError = 0;
+var accuracy;
+
+console.log("This is originText var: " + originText);
+console.log("This is changeOriginText var: " + changeOriginText);
+console.log("This is the current array selection: " + manyText[arrCount]);
+
+
+//Function for changing text for speed typing test
+function switchText() {
+    changeOriginText.innerHTML= manyText[arrCount];
+    arrCount++;
+    console.log("This is the current array selection: " + manyText[arrCount]);
+    
+    
+    if (arrCount >= manyText.length) {
+        arrCount = 0;
+    }
+    
+    reset();
+}
 
 // Add leading zero to numbers 9 or below (purely for aesthetics):
 function leadingZero(time) {
@@ -30,15 +59,34 @@ function runTimer() {
 // Match the text entered with the provided text on the page:
 function spellCheck() {
     let textEntered = testArea.value;
-    let originTextMatch = originText.substring(0, textEntered.length);
+    let originTextMatch = changeOriginText.innerHTML.substring(0, textEntered.length);
     
-    if (textEntered == originText) {
+    if (textEntered == changeOriginText.innerHTML) {
         clearInterval(interval);
         testWrapper.style.borderColor = "#008000";
+        
+        if (timer[0] >= 1) {
+            var completeTime = timer[0] + (timer[1] / 60);
+            console.log("This displays complete time by adding seconds to minutes: " + completeTime);
+            wordsPerMinCalc = Math.round((textEntered.length / 5) / completeTime);
+        } else {
+            var accurateMeasure = timer[1] / 60;
+            console.log("Divide seconds to get a fraction value for assesing WPM: " + accurateMeasure);
+            wordsPerMinCalc = Math.round((textEntered.length / 5) / accurateMeasure);
+        }
+        
+        console.log("WPM: " + wordsPerMinCalc);
+        wpmDisplay.innerHTML = wordsPerMinCalc;
+        
+        accuracy = Math.round(changeOriginText.innerHTML.length / typingError);
+        console.log("This is the accuracy: " + accuracy + "%");
+        accuracyDisplay.innerHTML = accuracy;
     } else {
         if (textEntered == originTextMatch) {
             testWrapper.style.borderColor = "#65CCf3";
         } else {
+            typingError++;
+            typingErrorDisplay.innerHTML = typingError;
             testWrapper.style.borderColor = "#8B0000";
         }
     }
@@ -64,10 +112,16 @@ function reset() {
     interval = null;
     timer = [0, 0, 0, 0];
     timerRunning = false;
+    wordsPerMinCalc;
+    typingError = 0;
+    accuracy;
     
     testArea.value = "";
     theTimer.innerHTML = "00:00:00";
     testWrapper.style.borderColor = "grey";
+    wpmDisplay.innerHTML = "---";
+    typingErrorDisplay.innerHTML = "---";
+    accuracyDisplay.innerHTML = "---";
     
     console.log("reset button has been pressed!");
 }
@@ -77,6 +131,14 @@ function reset() {
 testArea.addEventListener("keypress", start, false);
 testArea.addEventListener("keyup", spellCheck, false);
 resetButton.addEventListener("click", reset, false);
+console.log("Timer[2]: " + timer[2] + " Timer[3]: " + timer[3]);
+if (timer[2] >= 1) {
+    console.log("Timer[2]: " + timer[2] + " Timer[3]: " + timer[3]);
+    alert("cannot change text during test");
+} else {
+    console.log("Timer[2]: " + timer[2] + " Timer[3]: " + timer[3]);
+    changeOriginText.addEventListener("click", switchText, false);
+}
 
 
 //Functionality to add:
