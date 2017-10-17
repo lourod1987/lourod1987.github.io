6a599ecd5b39eba1e143555887b794732ccd078f
lourod1987 <louart87@gmail.com>
2017-10-17 00:42:10 -0400

    add temp fix for accuracy with word count adjustment... by 5 division

--- scripts/typingSpeedTest.js
+++ scripts/typingSpeedTest.js
@@ -82,13 +82,14 @@ function spellCheck() {
         console.log("WPM: " + wordsPerMinCalc);
         wpmDisplay.innerHTML = wordsPerMinCalc;
         
-        accuracy = (changeOriginText.innerHTML.length - typingError) / changeOriginText.innerHTML.length;
+        //temp fix for accuracy math.. need accurate word count for text; will be an involved process
+        accuracy = ((changeOriginText.innerHTML.length / 5) - typingError) / changeOriginText.innerHTML.length;
         console.log("Output of changeOriginText.innerHTML.length: " + changeOriginText.innerHTML.length);
         console.log("Initial accuracy math output: " + accuracy);
         if (accuracy === 0.1 || accuracy === -0.1) {
-            accuracy = accuracy * 1000;
+            accuracy = Math.round(accuracy * 1000);
         } else {
-            accuracy = accuracy * 100;
+            accuracy = Math.round(accuracy * 100);
         }
         console.log("This is the accuracy: " + accuracy + "%");
         accuracyDisplay.innerHTML = accuracy + "%";
