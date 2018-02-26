2e45e70dd2147e6bc0aee71fae0b5a7494980515
lourod1987 <louart87@gmail.com>
2018-02-26 14:13:07 -0600

    adding in all files and links necessary for standard portfolio/file layout scheme. Basic pixel art maker is now linked to portfolio under jquery. Additional style and functionality changes to come

---
+++ scripts/pixelArt.js
@@ -0,0 +1,73 @@
+//set a bool value to check if the grid has been previously created
+var ranAlready = false;
+//console.log(ranAlready);
+
+/*call functions on document load, this call click event on submit button calling event as an arg to prevent default functionality*/
+$(document).ready($("#submit").click(function (evt) {
+    
+    //prevent default functionality of submit button
+    evt.preventDefault();
+//    console.log("click worked");
+    
+    //call reset function and pass in ranAlready arg to reset grid
+    reset(ranAlready);
+    
+    //get height and width values of grid set by user
+    var height = $("#inputHeight").val();
+    var width = $("#inputWidth").val();
+    
+    //call makeGrid function and pass in height and width as args
+    makeGrid(height, width);
+    
+    //after makeGrid function has run set ranAlready to true
+    ranAlready = true;
+    
+//    console.log(ranAlready);
+//    console.log("this is the height: " + height);
+//    console.log("this is the width: " + width);    
+}));
+
+/*reset function used to check if makeGrid has been ran and if true clear grid before making a new one set ranAlready to false after*/
+function reset(ran) {
+    if (ranAlready === true) {
+        $('tr').remove();
+        $('td').remove();
+        ranAlready = false;
+    }
+}
+
+//make grid takes in two parameters for height and width of grid
+function makeGrid(rowCount, colCount) {
+//    console.log(" make grid call worked");
+//    console.log("this is the height within makeGrid: " + rowCount);
+//    console.log("this is the width within makeGrid: " + colCount);
+    
+    /*check to see if grid is equal to 1 and if so run code to create a single box else run code for specified terms*/
+    if (colCount <= 1 && rowCount <= 1) {
+        $('#pixelCanvas').append('<tr></tr>');
+        $('tr').append('<td></td>');
+    } else {
+        //loop over until height is reached appending tr tags each iteration
+        for (var row = 1; row <= rowCount; row++) {
+            console.log("Row count: " + row);
+            $('#pixelCanvas').append('<tr></tr>');
+            $('td').remove();
+            //loop over until width is reached appending td tags each iteration
+            for (var column = 1; column <= colCount; column++) {
+                console.log("Column count: " + column);
+                $('tr').append('<td></td>');
+            }    
+        }
+    }
+}
+
+//calling an event on click of the table based on the cell with a callback
+$('#pixelCanvas').on('click', 'td', function() {
+//    console.log("click in color is working");
+    //get color selected by user
+    var colorVal = $("#colorPicker").val();
+      
+    console.log("this is colorVal on click: " + colorVal);
+    //set background color of td cell clicked to currently selected color
+    $(this).css('background-color', colorVal) ;
+});
