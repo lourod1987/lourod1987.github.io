e84cd296f3a6a986b9f2064d29f90b297fe71194
lourod1987 <louart87@gmail.com>
2018-03-10 14:41:55 -0600

    built cat click v3 with new  layout and form entirely, style needs improvement, working on code improvements using MVC model for code

---
+++ cat-clickerMVC.js
@@ -0,0 +1,36 @@
+$(document).ready(function() {
+var model = {
+	[
+		{
+			name: Milo,
+			clickCount: 0	
+		},
+		{
+			name: Mittens,
+			clickCount: 0	
+		},
+		{
+			name: Cuddlers,
+			clickCount: 0
+		},
+		{
+			name: Boots,
+			clickCount: 0
+		},
+		{
+			name: Smiles,
+			clickCount: 0
+		}
+	]
+};
+	
+	
+var controller = {
+	
+};
+
+var view = {
+	
+};
+
+});
