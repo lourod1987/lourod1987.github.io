d4d4aedeef9808688ae6a21699bca5bdbbfd0a8d
lourod1987 <louart87@gmail.com>
2018-04-22 00:34:34 -0500

    Adding additional sort algorithms for practice and understand algoritm flows

--- scripts/bubble-sort.js
+++ scripts/bubble-sort.js
@@ -2,20 +2,21 @@ let uNums = [9, 8, 7, 5, 6, 3, 4, 1, 2, 10];
 
 
 const bubbleSort = (nums) => {
+	let x = 0;
 	do {
 		var swapped = false;
 		for (let i = 0; i < nums.length; i++) {
-			document.write(`<br>${nums}`);
-			
+			document.write(`<br>Iteration ${x}: ${nums}`);
+			x++;
 			if(nums[i] > nums[i + 1]) {
-				const temp = nums[i];
+				const TEMP = nums[i];
 				nums[i] = nums[i + 1];
-				nums[i + 1] = temp;
+				nums[i + 1] = TEMP;
 				swapped = true;
 			}
 		}
 	} while (swapped); 
-	return document.write(`<br>${nums}`);
+	return document.write(`<br>final ordered array: ${nums}`);
 }
 
 bubbleSort(uNums);
---
+++ scripts/merge-sort.js
@@ -0,0 +1,44 @@
+let uNums = [9, 8, 7, 5, 6, 3, 4, 1, 2, 10];
+
+
+const mergeSort = (nums) => {
+    if (nums.length < 2) {
+        return nums;
+    }
+    
+    const LENGTH = nums.length;
+    const MIDDLE = Math.floor(LENGTH / 2);
+    const LEFT = nums.slice(0, MIDDLE);
+    const RIGHT = nums.slice(MIDDLE, LENGTH);
+    
+    const sortedLeft = mergeSort(LEFT);
+    const sortedRight = mergeSort(RIGHT);
+    
+    document.write(`<br>Left half of array: ${sortedLeft}`);
+    document.write(`<br>Right half of array: ${sortedRight}`);
+    
+    return stitch(sortedLeft, sortedRight);
+};
+
+const stitch = (left, right) => {
+    const RESULTS = [];
+    
+    while (left.length && right.length) {
+        if (left[0] <= right[0]) {
+            RESULTS.push(left.shift());
+        } else {
+            RESULTS.push(right.shift());
+        }
+    }
+    
+    while (left.length) {
+        RESULTS.push(left.shift());
+    }
+    
+    while (right.length) {
+        RESULTS.push(right.shift());
+    }
+    return RESULTS;
+};
+
+mergeSort(uNums);
