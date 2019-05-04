4348383b166ddc3253e8dc7c6cf457867981dcce
lourod1987 <louart87@gmail.com>
2019-05-04 16:39:33 -0500

    Updates

---
+++ precache-manifest.a73484e0e97912aea44f130d25dacd9b.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = [
+  {
+    "revision": "80916a1df0faad5a1f5d390a08294551",
+    "url": "/static/media/mythicPreview.80916a1d.jpg"
+  },
+  {
+    "revision": "8c698640177b4a662e62",
+    "url": "/static/css/main.ed201d9f.chunk.css"
+  },
+  {
+    "revision": "9826f9f76f262a8a3572",
+    "url": "/static/js/1.9826f9f7.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "14ae64b62e1d69c6675de629e6fec359",
+    "url": "/static/media/GwGPreview.14ae64b6.jpg"
+  },
+  {
+    "revision": "a7f29d4e0a88614d6b1928012782991c",
+    "url": "/static/media/megamanPreview.a7f29d4e.jpg"
+  },
+  {
+    "revision": "0451e06652e82ccea9515a4132d63f9a",
+    "url": "/static/media/typingSpeedTestPreview.0451e066.jpg"
+  },
+  {
+    "revision": "8c698640177b4a662e62",
+    "url": "/static/js/main.8c698640.chunk.js"
+  },
+  {
+    "revision": "5dd8362ad473e42c424480830dbdfe4c",
+    "url": "/static/media/pixelArtPreview.5dd8362a.jpg"
+  },
+  {
+    "revision": "195a7f6883b26f4bd118e58f9a785463",
+    "url": "/static/media/Ask-Libbie-App-preview.195a7f68.png"
+  },
+  {
+    "revision": "5e2e5d1f52b780192cdef5aa90904507",
+    "url": "/static/media/Book-Reads-App.5e2e5d1f.png"
+  },
+  {
+    "revision": "4cdfbe3162c7edfb04fcb2258242fb74",
+    "url": "/static/media/lou_sketch.4cdfbe31.png"
+  },
+  {
+    "revision": "81fb39d2104f4ad31fbd60af905e1b3e",
+    "url": "/static/media/nasaNeutronStars_1920.81fb39d2.jpg"
+  },
+  {
+    "revision": "ef23a9df6a6671a70b2470d65176c9ba",
+    "url": "/index.html"
+  }
+];
